dd8519b9e1eed5fa45de8f7cf7e93433897729e3
kip <cyruskiprop254@gmail.com>
2020-07-18 00:28:23 +0300

    add calculator function and its test function

---
+++ src/calculator.js
@@ -0,0 +1,24 @@
+function Calculator(num1, num2) {
+  this.num1 = num1;
+  this.num2 = num2;
+}
+
+Calculator.prototype.add = function add() {
+  return this.num1 + this.num2;
+};
+
+Calculator.prototype.multiply = function multiply() {
+  return this.num1 * this.num2;
+};
+
+Calculator.prototype.divide = function divide() {
+  return this.num1 / this.num2;
+};
+
+Calculator.prototype.substract = function substract() {
+  return this.num1 - this.num2;
+};
+
+console.log(new Calculator(8, 4).add());
+
+module.export = Calculator;
