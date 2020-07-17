b140df57ac370e36bd8b269afb5c208cf391a782
Shubham Raj <shubham14p3@gmail.com>
2020-07-18 03:19:56 +0530

    add caesar and caesar test

--- src/caesar.js
+++ src/caesar.js
@@ -2,8 +2,8 @@ function caesar(str, key) {
   return str
     .toUpperCase()
     .replace(/[A-Z]/g, (c) =>
-      String.fromCharCode(((c.charCodeAt(0) - 65 + key) % 26) + 65)
-    );
+	  String.fromCharCode(((c.charCodeAt(0) - 65 + key) % 26) + 65)	
+	).toLowerCase()	  
 }
 
-module.export = caesar;
+module.exports = caesar;
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,5 +1,5 @@
 const caesar = require("../src/caesar");
 
 test("Return an encrypted string", () => {
-  expect(caesar("abc", 1).toBe("bcd"));
+  expect(caesar("abc", 1)).toBe("bcd");
 });
