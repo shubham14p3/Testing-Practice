8ff71524bfd37314346f31871785d0e5aef9216a
kip <cyruskiprop254@gmail.com>
2020-07-18 00:28:46 +0300

    add caesar function and its functio

---
+++ src/caesar.js
@@ -0,0 +1,9 @@
+function caesar(str, key) {
+  return str
+    .toUpperCase()
+    .replace(/[A-Z]/g, (c) =>
+      String.fromCharCode(((c.charCodeAt(0) - 65 + key) % 26) + 65)
+    );
+}
+
+module.export = caesar;
---
+++ test/caesar.test.js
@@ -0,0 +1,5 @@
+const caesar = require("../src/caesar");
+
+test("Return an encrypted string", () => {
+  expect(caesar("abc", 1).toBe("bcd"));
+});
